4abf911738c695ea78ee91ff4ae20d431191efef
zhangqx2010 <zhangqx2010@users.noreply.github.com>
2016-04-07 10:15:00 +0800

    Update dhost_change_name.py
    update sql

--- dhost_change_name.py
+++ dhost_change_name.py
@@ -16,22 +16,23 @@ try:
     conn=mysql.connector.connect(host='localhost',user='zabbix',passwd='',db='zabbix')
     cur=conn.cursor()
     ### get discovered hostid and hostname in database
-    cur.execute('''select hosts.host, hosts.name
+    cur.execute('''select hosts.hostid, hosts.host, hosts.name
                    from hosts_groups
                    inner join hosts
-                   on hosts_groups.groupid=5 and hosts_groups.hostid=hosts.hostid;''')
+                   on hosts_groups.groupid=5 and hosts_groups.hostid=hosts.hostid and hosts.host=hosts.name;''')
     rows = cur.fetchall()
     if (len(rows) == 0): exit()
     for row in rows:
-        ip = row[0]
-        name = row[1]
+        hostid = row[0]
+        ip = row[1]
+        name = row[2]
         #check if hostname is an ip address
         #discovered VM's hostname is an UUID not an address
         if not pat.match(name): continue
         command = str("zabbix_get -s " + name + " -k agent.hostname")
-        dhname=os.popen(command).readlines()[0].strip('\n')
+        dhname = os.popen(command).readlines()[0].strip('\n')
         #update database
-        update_sql=str("update hosts set name='" + dhname + "' where host='" + ip + "'")
+        update_sql = str("update hosts set name='" + dhname + "' where host='" + hostid + "'")
         cur.execute(update_sql)
     cur.close()
     conn.commit()
